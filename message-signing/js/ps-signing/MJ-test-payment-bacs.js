396a20ad27aef44f7f66c492ad3e80542f3c737d
Marjorie Jones <marjorie.jones@starlingbank.com>
2021-04-21 16:18:51 +0100

    Slightly different variants on successful calls

--- message-signing/js/ps-signing/MJ-test-payment-bacs.js
+++ message-signing/js/ps-signing/MJ-test-payment-bacs.js
@@ -382,7 +382,7 @@ const putMandateValid7 = async () => {
     const action = '/*** (F.1.7) putMandate - PB, acc, addr, mandate valid, Saff\'s structure, my data with spaces and apostrophes ***/';
     const data = {
         "originatorServiceUserNumber": "123456",
-        "originatorReference": "MJ's test ref",  // Same length as Saff's, with apostrophe
+        "originatorReference": "MJ#s test ref",  // Same length as Saff's, with ~apostrophe but #
         "originatorName": "ORIGINATOR"
     };
 
